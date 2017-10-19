52a3f5134b738cf79e9d1545996e39edb1bcdd9a
Andy Boyle <andy@sudostream.io>
2017-10-19 21:22:08 +0100

    Scala Js hooks into CSS now

---
+++ client/src/main/scala/timetoteach/model/Subjects.scala
@@ -0,0 +1,22 @@
+package timetoteach.model
+
+object Subjects {
+  val values = Set(
+    Subject("subject-golden-time"),
+    Subject("subject-other"),
+    Subject("subject-ict"),
+    Subject("subject-music"),
+    Subject("subject-drama"),
+    Subject("subject-health"),
+    Subject("subject-teacher-covertime"),
+    Subject("subject-assembly-button"),
+    Subject("subject-reading"),
+    Subject("subject-spelling"),
+    Subject("subject-writing"),
+    Subject("subject-maths"),
+    Subject("subject-topic"),
+    Subject("subject-physical-education")
+  )
+}
+
+case class Subject(value: String)
--- client/src/main/scala/timetoteach/screens/ClassTimetable.scala
+++ client/src/main/scala/timetoteach/screens/ClassTimetable.scala
@@ -1,12 +1,37 @@
 package timetoteach.screens
 
+import org.scalajs.dom
+import org.scalajs.dom.raw
+import org.scalajs.dom.raw.HTMLDivElement
+import timetoteach.model.{Subject, Subjects}
+
 object ClassTimetable {
 
+  var currentlySelectSubject: Option[Subject] = None
 
+  def addListenerToAllSubjectButtons(): Unit = {
+    for (subject <- Subjects.values) {
+      val subjectElement = dom.document.getElementById(subject.value)
+      subjectElement.addEventListener("click", (e: dom.Event) => {
 
+        e.currentTarget match {
+          case subjectDiv: HTMLDivElement =>
+            subjectDiv.style.textShadow = "1px 1px #222"
+            subjectDiv.style.borderColor = "white"
+            subjectDiv.style.border = "bold"
+            subjectDiv.style.color = "white"
+            subjectDiv.style.fontWeight = "bold"
+            subjectDiv.style.fontSize = "largest"
+
+          case _ =>
+        }
+
+      })
+    }
+  }
 
   def loadClassTimetableJavascript(): Unit = {
-
+    addListenerToAllSubjectButtons()
   }
 
 }
