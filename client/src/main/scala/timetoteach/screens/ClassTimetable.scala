9082ae4a80e729bf3f79a0daf2640b37d8f24390
Andy Boyle <andy@sudostream.io>
2017-10-24 16:36:52 +0100

    Classtimetable: Selecting subjects looking more vivid. Can also drag subjects now

--- client/src/main/scala/timetoteach/screens/ClassTimetable.scala
+++ client/src/main/scala/timetoteach/screens/ClassTimetable.scala
@@ -1,13 +1,13 @@
 package timetoteach.screens
 
 import org.scalajs.dom
-import org.scalajs.dom.raw
 import org.scalajs.dom.raw.HTMLDivElement
 import timetoteach.model.{Subject, Subjects}
 
 object ClassTimetable {
 
   var currentlySelectSubject: Option[Subject] = None
+  var orginalColour = ""
 
   def addListenerToAllSubjectButtons(): Unit = {
     for (subject <- Subjects.values) {
@@ -16,20 +16,54 @@ object ClassTimetable {
 
         e.currentTarget match {
           case subjectDiv: HTMLDivElement =>
-            subjectDiv.style.textShadow = "1px 1px #222"
-            subjectDiv.style.borderColor = "white"
-            subjectDiv.style.border = "bold"
-            subjectDiv.style.color = "white"
-            subjectDiv.style.fontWeight = "bold"
-            subjectDiv.style.fontSize = "largest"
+            val justSelectedSubject = Subject(subjectDiv.id)
+            if (currentlySelectSubject.isDefined) {
+              if (currentlySelectSubject.get == justSelectedSubject) {
+                resetDiv(subjectDiv)
+                currentlySelectSubject = None
+              } else {
+                val currentlySelectedDiv =
+                  dom.document.getElementById(currentlySelectSubject.get.value).asInstanceOf[HTMLDivElement]
+                resetDiv(currentlySelectedDiv)
+                selectThisElement(subjectDiv, justSelectedSubject)
+              }
+            } else {
+              selectThisElement(subjectDiv, justSelectedSubject)
+            }
 
           case _ =>
         }
 
       })
     }
+
   }
 
+  private def resetDiv(subjectDiv: HTMLDivElement) = {
+    val parent = subjectDiv.parentNode.asInstanceOf[HTMLDivElement]
+    parent.style.border = ""
+    parent.style.paddingLeft = "15px"
+    parent.style.paddingRight = "15px"
+
+    subjectDiv.style.textShadow = ""
+    subjectDiv.style.color = orginalColour
+    subjectDiv.style.fontWeight = "normal"
+    subjectDiv.style.fontSize = "medium"
+  }
+  private def selectThisElement(subjectDiv: HTMLDivElement, justSelectedSubject: Subject) = {
+
+    val parent = subjectDiv.parentNode.asInstanceOf[HTMLDivElement]
+    parent.style.border = "5px solid white"
+    parent.style.paddingLeft = "0"
+    parent.style.paddingRight = "0"
+
+    subjectDiv.style.textShadow = "1px 1px #222"
+    orginalColour = subjectDiv.style.color
+    subjectDiv.style.color = "white"
+    subjectDiv.style.fontWeight = "bold"
+    subjectDiv.style.fontSize = "largest"
+    currentlySelectSubject = Some(justSelectedSubject)
+  }
   def loadClassTimetableJavascript(): Unit = {
     addListenerToAllSubjectButtons()
   }
