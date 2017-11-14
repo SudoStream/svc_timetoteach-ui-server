71a3432e855c75aa16c1f78fbd7e1aa0cc0fb7b2
Andy Boyle <andy@sudostream.io>
2017-11-14 16:34:49 +0000

    better stack trace

--- server/app/controllers/serviceproxies/UserWriterServiceProxyImpl.scala
+++ server/app/controllers/serviceproxies/UserWriterServiceProxyImpl.scala
@@ -43,7 +43,7 @@ class UserWriterServiceProxyImpl @Inject()(schoolReader: SchoolReaderServiceProx
         school => school.id -> convertLocalSchoolToMessageSchool(school)
       } toMap
     case Failure(t) =>
-      logger.error(s"Failed to get schools on loadup. ${t.toString} \n\n ${t.getStackTrace.toString}")
+      logger.error(s"Failed to get schools on loadup. ${t.toString} \n\n ${t.getStackTrace.map(_.toString)}")
   }
 
   private var theSchools: Map[String, io.sudostream.timetoteach.messages.systemwide.model.School] = Map.empty
