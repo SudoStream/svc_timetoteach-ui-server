2871370962a8be64d26340ce350aaad9c2020d6c
Andy Boyle <andy@sudostream.io>
2018-01-16 19:49:11 +0000

    0.0.1-135 : Adding new class - checkpoint 1

--- client/src/main/scala/timetoteach/addnewclass/AddNewClassJsScreen.scala
+++ client/src/main/scala/timetoteach/addnewclass/AddNewClassJsScreen.scala
@@ -1,17 +1,41 @@
 package timetoteach.addnewclass
 
 import org.scalajs.dom
-import org.scalajs.dom.raw.HTMLButtonElement
+import org.scalajs.dom.raw.{HTMLButtonElement, HTMLDivElement}
+
+import scala.scalajs.js.Dynamic.global
+import scalatags.JsDom.all.{`class`, div, _}
 
 object AddNewClassJsScreen {
 
-  def addNewClassGroupsDiv(): Unit = {
-    val addNewGroupButton = dom.document.getElementById("add-new-class-groups-div").asInstanceOf[HTMLButtonElement]
-    if ( addNewGroupButton != null) {
-      // If this button is clicked
+  private var currentGroups = List()
+  private var groupCounter = 0
 
-      // Add a new line with drop downs etc
-      // e.g. [Enter Group Name] / [Select Group Type] / [Select Group Level]
+  def addNewClassGroupsDiv(): Unit = {
+    val addNewGroupButton = dom.document.getElementById("add-new-class-groups-button").asInstanceOf[HTMLButtonElement]
+    if (addNewGroupButton != null) {
+      // If this button is clicked
+      addNewGroupButton.addEventListener("click", (e: dom.Event) => {
+        global.console.log("Add new group ...")
+
+        // Add a new line with drop downs etc
+        // e.g. [Enter Group Name] / [Select Group Type] / [Select Group Level]
+        val newAddGroupRow = div(`class` := "row add-new-group-row")(
+          div(`class` := "col-sm-4")(input(
+            `type` := "text",
+            `class` := "form-control",
+            `name` := s"addGroup${currentGroups.size + 1}",
+            placeholder := s"Enter group name (${currentGroups.size + 1})"
+          )),
+          div(`class` := "col-sm-4")(div("hello2")),
+          div(`class` := "col-sm-4")(div("hello3"))
+        )
+
+        val newGroupsDiv = dom.document.getElementById("add-new-class-groups-div").asInstanceOf[HTMLDivElement]
+        newGroupsDiv.innerHTML = newGroupsDiv.innerHTML + newAddGroupRow.toString()
+      })
+
+
     } else {
       println("ERROR : There is no button called add-new-class-groups-div")
     }
