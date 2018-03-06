2d1c3c89dafb927deb13144d395c09c24ae4adba
Andy Boyle <andy@sudostream.io>
2018-03-06 15:25:59 +0000

    0.0.1-305 : Termly Class Plans navigation somewhat aware of lower levels now

--- server/app/models/timetoteach/ui/ClassIdContext.scala
+++ server/app/models/timetoteach/ui/ClassIdContext.scala
@@ -5,4 +5,4 @@ sealed trait ClassIdContext
 case object CLASS_SETUP extends ClassIdContext
 case object CLASS_TIMETABLE extends ClassIdContext
 case object CLASS_TERMLY_PLANS extends ClassIdContext
-
+case object CLASS_TERMLY_PLANS_LOWER extends ClassIdContext
