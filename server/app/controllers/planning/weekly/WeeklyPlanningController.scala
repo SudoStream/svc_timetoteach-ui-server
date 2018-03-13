ad04aaeb9671e4c676085f1dec69b858d48817ca
Andy Boyle <andy@sudostream.io>
2018-03-13 16:10:54 +0000

    0.0.1-363 : [timetoteach/#259] Weekly Planning : Add 3rd Level Nav Buttons - basic start  :: DONE

--- server/app/controllers/planning/weekly/WeeklyPlanningController.scala
+++ server/app/controllers/planning/weekly/WeeklyPlanningController.scala
@@ -3,14 +3,14 @@ package controllers.planning.weekly
 import be.objectify.deadbolt.scala.DeadboltActions
 import controllers.serviceproxies.{ClassTimetableReaderServiceProxyImpl, PlanningReaderServiceProxy, UserReaderServiceProxyImpl}
 import curriculum.scotland.EsOsAndBenchmarksBuilderImpl
+import duplicate.model.ClassDetails
 import javax.inject.{Inject, Singleton}
-import models.timetoteach.CookieNames
-import play.api.mvc.{AbstractController, Action, ControllerComponents}
+import models.timetoteach.{CookieNames, TimeToTeachUserId}
+import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents}
 import security.MyDeadboltHandler
 import utils.TemplateUtils.getCookieStringFromRequest
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
 
 @Singleton
 class WeeklyPlanningController @Inject()(
@@ -23,19 +23,49 @@ class WeeklyPlanningController @Inject()(
                                         ) extends AbstractController(cc)
 {
 
-  def weeklyPlanning = deadbolt.SubjectPresent()() { authRequest =>
+  def weeklyPlanning(classId: String): Action[AnyContent] = deadbolt.SubjectPresent()() { authRequest =>
     val userPictureUri = getCookieStringFromRequest(CookieNames.socialNetworkPicture, authRequest)
     val userFirstName = getCookieStringFromRequest(CookieNames.socialNetworkGivenName, authRequest)
     val userFamilyName = getCookieStringFromRequest(CookieNames.socialNetworkFamilyName, authRequest)
+    val tttUserId = getCookieStringFromRequest(CookieNames.timetoteachId, authRequest).getOrElse("NO ID")
+    val eventualClasses = classTimetableReaderProxy.extractClassesAssociatedWithTeacher(TimeToTeachUserId(tttUserId))
 
-    Future {
-      Ok(views.html.planning.weekly.homeWeeklyPlanning(new MyDeadboltHandler(userReader),
-        "Weekly Planning",
-        userPictureUri,
-        userFirstName,
-        userFamilyName)
-      )
-    }
+    for {
+      classes <- eventualClasses
+      classDetailsList = classes.filter(theClass => theClass.id.id == classId)
+      maybeClassDetails: Option[ClassDetails] = classDetailsList.headOption
+      if maybeClassDetails.isDefined
+      classDetails = maybeClassDetails.get
+    } yield Ok(views.html.planning.weekly.weeklyView(
+      new MyDeadboltHandler(userReader),
+      userPictureUri,
+      userFirstName,
+      userFamilyName,
+      classDetails
+    ))
+  }
+
+  def weeklyViewOfWeeklyPlanning(classId: String): Action[AnyContent] = deadbolt.SubjectPresent()() { authRequest =>
+    val userPictureUri = getCookieStringFromRequest(CookieNames.socialNetworkPicture, authRequest)
+    val userFirstName = getCookieStringFromRequest(CookieNames.socialNetworkGivenName, authRequest)
+    val userFamilyName = getCookieStringFromRequest(CookieNames.socialNetworkFamilyName, authRequest)
+    val tttUserId = getCookieStringFromRequest(CookieNames.timetoteachId, authRequest).getOrElse("NO ID")
+    val eventualClasses = classTimetableReaderProxy.extractClassesAssociatedWithTeacher(TimeToTeachUserId(tttUserId))
+
+    for {
+      classes <- eventualClasses
+      classDetailsList = classes.filter(theClass => theClass.id.id == classId)
+      maybeClassDetails: Option[ClassDetails] = classDetailsList.headOption
+      if maybeClassDetails.isDefined
+      classDetails = maybeClassDetails.get
+    } yield Ok(views.html.planning.weekly.weeklyView(
+      new MyDeadboltHandler(userReader),
+      userPictureUri,
+      userFirstName,
+      userFamilyName,
+      classDetails
+    ))
+
   }
 
 
--- server/app/models/timetoteach/ui/ClassIdContext.scala
+++ server/app/models/timetoteach/ui/ClassIdContext.scala
@@ -1,12 +1,11 @@
 package models.timetoteach.ui
 
 sealed trait ClassIdContext
-
 case object CLASS_SETUP extends ClassIdContext
-
 case object CLASS_TIMETABLE extends ClassIdContext
-
 case object CLASS_TERMLY_PLANS extends ClassIdContext
 case object CLASS_TERMLY_PLANS_LOWER extends ClassIdContext
-
 case object CLASS_WEEKLY_PLANS extends ClassIdContext
+
+sealed trait ClassIdSubContext
+case object WEEKLY_VIEW extends ClassIdSubContext
