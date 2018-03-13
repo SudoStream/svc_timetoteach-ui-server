569d4439c0e40be112bfd34b5308cf99ebe4e642
Andy Boyle <andy@sudostream.io>
2018-03-13 13:17:44 +0000

    0.0.1-360 : [timetoteach/#257] Weekly Plans : Project Setup - Added Html to main app template to make it easier to add 2nd level nav items

--- server/app/models/timetoteach/ui/ClassIdContext.scala
+++ server/app/models/timetoteach/ui/ClassIdContext.scala
@@ -3,6 +3,10 @@ package models.timetoteach.ui
 sealed trait ClassIdContext
 
 case object CLASS_SETUP extends ClassIdContext
+
 case object CLASS_TIMETABLE extends ClassIdContext
+
 case object CLASS_TERMLY_PLANS extends ClassIdContext
 case object CLASS_TERMLY_PLANS_LOWER extends ClassIdContext
+
+case object CLASS_WEEKLY_PLANS extends ClassIdContext
