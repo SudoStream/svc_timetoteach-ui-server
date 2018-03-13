de56494ecf1702ee28dc912432a6d4a3c570167f
Andy Boyle <andy@sudostream.io>
2018-03-13 17:04:24 +0000

    0.0.1-364 : [timetoteach/#260] Weekly Planning : Weekly View - Ball now rolling

--- server/app/controllers/planning/weekly/WeeklyPlanningController.scala
+++ server/app/controllers/planning/weekly/WeeklyPlanningController.scala
@@ -1,16 +1,24 @@
 package controllers.planning.weekly
 
+import java.time.LocalTime
+
 import be.objectify.deadbolt.scala.DeadboltActions
 import controllers.serviceproxies.{ClassTimetableReaderServiceProxyImpl, PlanningReaderServiceProxy, UserReaderServiceProxyImpl}
 import curriculum.scotland.EsOsAndBenchmarksBuilderImpl
 import duplicate.model.ClassDetails
+import io.sudostream.timetoteach.messages.systemwide.model.UserPreferences
 import javax.inject.{Inject, Singleton}
+import models.timetoteach.classtimetable.SchoolDayTimes
 import models.timetoteach.{CookieNames, TimeToTeachUserId}
+import play.api.Logger
 import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents}
 import security.MyDeadboltHandler
+import shared.model.classtimetable.WwwClassId
+import shared.util.LocalTimeUtil
 import utils.TemplateUtils.getCookieStringFromRequest
 
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 @Singleton
 class WeeklyPlanningController @Inject()(
@@ -23,27 +31,16 @@ class WeeklyPlanningController @Inject()(
                                         ) extends AbstractController(cc)
 {
 
-  def weeklyPlanning(classId: String): Action[AnyContent] = deadbolt.SubjectPresent()() { authRequest =>
-    val userPictureUri = getCookieStringFromRequest(CookieNames.socialNetworkPicture, authRequest)
-    val userFirstName = getCookieStringFromRequest(CookieNames.socialNetworkGivenName, authRequest)
-    val userFamilyName = getCookieStringFromRequest(CookieNames.socialNetworkFamilyName, authRequest)
-    val tttUserId = getCookieStringFromRequest(CookieNames.timetoteachId, authRequest).getOrElse("NO ID")
-    val eventualClasses = classTimetableReaderProxy.extractClassesAssociatedWithTeacher(TimeToTeachUserId(tttUserId))
+  private val logger: Logger.type = Logger
 
-    for {
-      classes <- eventualClasses
-      classDetailsList = classes.filter(theClass => theClass.id.id == classId)
-      maybeClassDetails: Option[ClassDetails] = classDetailsList.headOption
-      if maybeClassDetails.isDefined
-      classDetails = maybeClassDetails.get
-    } yield Ok(views.html.planning.weekly.weeklyView(
-      new MyDeadboltHandler(userReader),
-      userPictureUri,
-      userFirstName,
-      userFamilyName,
-      classDetails
-    ))
-  }
+  private val defaultSchoolDayTimes = SchoolDayTimes(
+    schoolDayStarts = LocalTime.of(9, 0),
+    morningBreakStarts = LocalTime.of(10, 30),
+    morningBreakEnds = LocalTime.of(10, 45),
+    lunchStarts = LocalTime.of(12, 0),
+    lunchEnds = LocalTime.of(13, 0),
+    schoolDayEnds = LocalTime.of(15, 0)
+  )
 
   def weeklyViewOfWeeklyPlanning(classId: String): Action[AnyContent] = deadbolt.SubjectPresent()() { authRequest =>
     val userPictureUri = getCookieStringFromRequest(CookieNames.socialNetworkPicture, authRequest)
@@ -51,6 +48,27 @@ class WeeklyPlanningController @Inject()(
     val userFamilyName = getCookieStringFromRequest(CookieNames.socialNetworkFamilyName, authRequest)
     val tttUserId = getCookieStringFromRequest(CookieNames.timetoteachId, authRequest).getOrElse("NO ID")
     val eventualClasses = classTimetableReaderProxy.extractClassesAssociatedWithTeacher(TimeToTeachUserId(tttUserId))
+    val futureUserPrefs: Future[Option[UserPreferences]] = userReader.getUserPreferences(TimeToTeachUserId(tttUserId))
+    val futureSchoolDayTimes: Future[SchoolDayTimes] = futureUserPrefs map {
+      case Some(userPrefs) =>
+        val schoolTimes = userPrefs.allSchoolTimes.head
+        logger.debug(s"classTimetable() : schoolTimes : ${schoolTimes.toString}")
+        SchoolDayTimes(
+          schoolDayStarts = LocalTimeUtil.convertStringTimeToLocalTime(schoolTimes.schoolStartTime).getOrElse(
+            defaultSchoolDayTimes.schoolDayStarts),
+          morningBreakStarts = LocalTimeUtil.convertStringTimeToLocalTime(schoolTimes.morningBreakStartTime).getOrElse(
+            defaultSchoolDayTimes.morningBreakStarts),
+          morningBreakEnds = LocalTimeUtil.convertStringTimeToLocalTime(schoolTimes.morningBreakEndTime).getOrElse(
+            defaultSchoolDayTimes.morningBreakEnds),
+          lunchStarts = LocalTimeUtil.convertStringTimeToLocalTime(schoolTimes.lunchStartTime).getOrElse(
+            defaultSchoolDayTimes.lunchStarts),
+          lunchEnds = LocalTimeUtil.convertStringTimeToLocalTime(schoolTimes.lunchEndTime).getOrElse(
+            defaultSchoolDayTimes.lunchEnds),
+          schoolDayEnds = LocalTimeUtil.convertStringTimeToLocalTime(schoolTimes.schoolEndTime).getOrElse(
+            defaultSchoolDayTimes.schoolDayEnds)
+        )
+      case None => defaultSchoolDayTimes
+    }
 
     for {
       classes <- eventualClasses
@@ -58,12 +76,19 @@ class WeeklyPlanningController @Inject()(
       maybeClassDetails: Option[ClassDetails] = classDetailsList.headOption
       if maybeClassDetails.isDefined
       classDetails = maybeClassDetails.get
+      wwwClassTimetableFuture = classTimetableReaderProxy.
+        readClassTimetable(TimeToTeachUserId(tttUserId), WwwClassId(classDetails.id.id))
+      schoolDayTimes <- futureSchoolDayTimes
+      maybeWwwClassTimetable <- wwwClassTimetableFuture
+      if maybeWwwClassTimetable.isDefined
     } yield Ok(views.html.planning.weekly.weeklyView(
       new MyDeadboltHandler(userReader),
       userPictureUri,
       userFirstName,
       userFamilyName,
-      classDetails
+      classDetails,
+      schoolDayTimes,
+      maybeWwwClassTimetable.get
     ))
 
   }
