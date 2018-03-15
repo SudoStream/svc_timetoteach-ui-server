f30ae0be7ba3b469e73114f7582820ffc9cad293
Andy Boyle <andy@sudostream.io>
2018-03-15 10:06:48 +0000

    0.0.1-375 : [timetoteach/#260] Weekly Planning : Weekly View - Prettier with top/bottom border only coloured

--- server/app/models/timetoteach/ui/ClassIdContext.scala
+++ server/app/models/timetoteach/ui/ClassIdContext.scala
@@ -9,3 +9,4 @@ case object CLASS_WEEKLY_PLANS extends ClassIdContext
 
 sealed trait ClassIdSubContext
 case object WEEKLY_VIEW extends ClassIdSubContext
+case object WEEKLY_PLANNING extends ClassIdSubContext
